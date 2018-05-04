9688b9ced6244946606809fe3667e1c3d8409b0a
ICE <ice@lab06m34.nrc.ice.ufjf.br>
2018-05-04 19:45:16 -0300

    Criação da classe abstrata de Teste.

--- src/test/java/com/grupo3/TestarLogin.java
+++ src/test/java/com/grupo3/TestarLogin.java
@@ -1,5 +1,6 @@
 
 package com.grupo3;
+import com.util.Teste;
 import org.junit.After;
 import org.junit.AfterClass;
 import static org.junit.Assert.assertFalse;
@@ -7,23 +8,21 @@ import static org.junit.Assert.assertTrue;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.PageFactory;
 
 public class TestarLogin extends Teste{
     
     
     private final String url = "https://accounts.google.com/signup/v2/webcreateaccount?service=accountsettings&continue=https%3A%2F%2Fmyaccount.google.com%2Fintro%3Futm_source%3DOGB%26utm_medium%3Dapp&flowName=GlifWebSignIn&flowEntry=SignUp";
-    private final String url2 = Teste.dominio;
+    private final String url2 = Teste.DOMINIO;
     @Before
     public void before() {
-        super.conecta(url);
+        super.conectar(url);
     }
 
     @After
     public void after() {
-        encerra();
+        encerrar();
     }
 
     @BeforeClass
---
+++ src/test/java/com/util/Teste.java
@@ -0,0 +1,30 @@
+package com.util;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public abstract class Teste {
+
+    private WebDriver driver;
+    public static final String DOMINIO = "https://atlantis.isti.cnr.it:5000/";
+    
+    public void conectar(String url) {
+        /**
+         * se caso alguem quiser usar diferente
+         */
+        System.setProperty("webdriver.gecko.driver", "drivers/geckodriver");
+        driver = new FirefoxDriver();
+        /*System.setProperty("webdriver.chrome.driver", "drivers/chrome");
+        driver = new ChromeDriver();*/
+        driver.get(url);
+    }
+
+    public void encerrar() {
+        driver.close();
+    }
+
+    public WebDriver getDriver() {
+        return this.driver;
+    }
+}
