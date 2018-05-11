3912fc828be62efb9e58d4a3133e5070b2d345f5
Luis Felipe <luis.felipe.almeida.nascimento@gmail.com>
2018-05-11 20:41:44 -0300

    Finalizado os testes de cadastro atividade

--- src/test/java/testes/TesteArquivo.java
+++ src/test/java/testes/TesteArquivo.java
@@ -5,6 +5,25 @@
  */
 package testes;
 
+import java.util.Date;
+import java.util.Random;
+import util.Teste;
+import org.junit.After;
+import org.junit.AfterClass;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import pojos.PojoPaginaCursosProfessor;
+import pojos.PojoPaginaInicial;
+import pojos.PojoPaginaInicialProfessor;
 /**
  *
  * @author luisfelipe
